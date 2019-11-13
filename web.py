0cc14282dc0856809e40ccde73c0246e5270824c
Uros Ogrizovic <uros.ogrizovic671@gmail.com>
2019-11-13 21:38:14 +0100

    Implement submitDrawing() function

--- web.py
+++ web.py
@@ -1,3 +1,5 @@
+import base64
+
 from flask import Flask, escape, request, send_file, render_template
 import display_image
 
@@ -10,6 +12,18 @@ def hello():
     # display_image.save_as_image(img)
     return render_template('index.html')
     # return send_file('image.png')
+
+@app.route("/saveimage", methods=['POST'])
+def save_image():
+    try:
+        base64Img = request.form['javascript_data']
+        base64Img = base64Img.replace("data:image/octet-stream;base64,", "")
+        imgdata = base64.b64decode(base64Img)
+        with open('img.png', 'wb') as f:
+            f.write(imgdata)
+        return 'ok'
+    except:
+        return 'fail'
 
 if __name__ == "__main__":
     app.run(debug=True)
